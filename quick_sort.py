2898f4cf3181f078332b05ac332b354ab1d1be48
yang <yang@yangdeMBP.lan>
2016-11-25 10:58:06 +0800

    correct quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-
+""" wrong quick  sort
 def quick_sort_helper(l,left, right):
     if((right-left)>0):
         key = l[left]
@@ -15,9 +15,23 @@ def quick_sort_helper(l,left, right):
         print l
         quick_sort_helper(l, left, flag-1)
         quick_sort_helper(l, flag, right)
-
+"""
 def quick_sort(l):
-    quick_sort_helper(l,0,len(l)-1)
+    #quick_sort_helper(l,0,len(l)-1)
+    quick_sort_helper1(l,0,len(l)-1)
+
+def quick_sort_helper1(l, left, right):
+    if((right-left)>0):
+        key = l[left]
+        flag = left+1
+        for i in range(left+1, right+1):
+            if(l[i] < key):
+                l[i], l[flag] = l[flag], l[i]
+                flag += 1
+        l[left], l[flag-1] = l[flag-1], l[left]
+        print l
+        quick_sort_helper1(l, left, flag-1)
+        quick_sort_helper1(l, flag, right)
 
 
 b_list = [54, 26, 93, 17, 77]
